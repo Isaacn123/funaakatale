96f40214622bea25e7a8c49e64aee0abce0785fd
isaac <nsambai72@gmail.com>
2021-10-08 12:35:16 +0300

    dependecies update

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->na
 Route::resource('category', CategoryController::class);
 Route::resource('sub-category', subCategoryController::class);
 Route::resource('/dash-b', DashboardController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
